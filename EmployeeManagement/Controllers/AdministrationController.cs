aa895422a60e9e00c655dbf3ec9fdfe1c64a3eff
JanBe1 <jan.bernardzik@gmail.com>
2021-10-05 11:11:18 +0200

    Added role removal functionality.

--- EmployeeManagement/Controllers/AdministrationController.cs
+++ EmployeeManagement/Controllers/AdministrationController.cs
@@ -191,6 +191,35 @@ namespace EmployeeManagement.Controllers
             return RedirectToAction("EditRole", new { Id = roleId });
         }
 
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            var role = await roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+                return View("NotFound");
+            }
+            else
+            {
+                var result = await roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles");
+                }
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+                return View("ListRoles");
+            }
+
+        }
+
+
+
         [HttpGet]
         public IActionResult ListUsers() 
         {
@@ -261,7 +290,7 @@ namespace EmployeeManagement.Controllers
                 var result = await userManager.DeleteAsync(user);
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("ListRoles");
+                    return RedirectToAction("ListUsers");
                 }
                 foreach (var error in result.Errors)
                 {
